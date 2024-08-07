779e64c1abc9798acbe0163fd3a16894d9d647a6
Joep Boumans <joep_boumans@live.nl>
2024-08-07 15:19:05 +0200

    Header building by direct linking

--- main.cpp
+++ main.cpp
@@ -1,10 +1,10 @@
+#include "src/custom_math.h"
 #include <iostream>
 
 #ifndef SYSTEM
 #define SYSTEM "NOT FOUND"
 #endif // !SYSTEM
 
-auto sum(int a, int b) { return a + b; }
 int main() {
   std::cout << "Hello World\n";
   std::cout << "Sum of 2 + 1 :" << sum(2, 1) << std::endl;
---
+++ src/custom_math.h
@@ -0,0 +1,5 @@
+#ifndef _CUSTOM_MATH_H
+#define _CUSTOM_MATH_H
+
+auto sum(int a, int b) { return a + b; }
+#endif // !_CUSTOM_MATH_H
